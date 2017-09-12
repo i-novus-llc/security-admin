089ba2356e650a4aaaab536a5837b6f340944379
rgalina <rgalina@i-novus.ru>
2017-09-12 20:31:40 +0300

    [rf] renaming object refs

--- security-admin-api/src/main/java/net/n2oapp/security/admin/service/UserService.java
+++ security-admin-api/src/main/java/net/n2oapp/security/admin/service/UserService.java
@@ -7,9 +7,9 @@ import net.n2oapp.security.admin.model.User;
  */
 public interface UserService {
 
-    Integer create(User role);
+    Integer create(User user);
 
-    Integer update(User role);
+    Integer update(User user);
 
     void delete(Integer id);
 }
