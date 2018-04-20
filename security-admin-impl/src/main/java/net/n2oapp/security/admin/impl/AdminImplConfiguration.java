4f6cc69dc1d508a33bed40b2bc95ba5305bc73c2
rgalina <rgalina@i-novus.ru>
2018-04-20 17:37:42 +0300

    [nf] Настройки отправки email

--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/AdminImplConfiguration.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/AdminImplConfiguration.java
@@ -12,8 +12,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Properties;
 
 @Configuration
 @PropertySource("classpath:security.properties")
@@ -21,6 +25,27 @@ public class AdminImplConfiguration {
 
     @Value("${sec.password.generate.length}")
     private Integer passwordGeneratorLength;
+
+    @Value("${sec.spring.mail.host}")
+    private String mailHost;
+
+    @Value("${sec.spring.mail.port}")
+    private int mailPort;
+
+    @Value("${sec.spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${sec.spring.mail.password}")
+    private String mailPassword;
+
+    @Value("${sec.spring.mail.properties.mail.smtp.auth.enable}")
+    private Boolean mailSmtpAuthEnable;
+
+    @Value("${sec.spring.mail.properties.mail.smtp.starttls.enable}")
+    private Boolean mailSmtpStarttlsEnable;
+
+    @Value("${sec.spring.mail.properties.mail.smtp.auth}")
+    private Boolean mailSmtpAuth;
 
     @Bean
     public UserService userService(UserRepository userRepository, RoleRepository roleRepository, SsoUserRoleProvider ssoUserRoleProvider) {
@@ -48,5 +73,22 @@ public class AdminImplConfiguration {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(mailHost);
+        mailSender.setPort(mailPort);
+
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", mailSmtpAuth);
+        props.put("mail.smtp.starttls.enable", mailSmtpStarttlsEnable);
+
+        return mailSender;
     }
 }
