977b6294f074c3887bb8b467abf54a45292017ce
iryabov <iryabov@i-novus.ru>
2018-04-26 15:16:38 +0300

    [RF] Использование адреса rest через аннотацию

--- security-admin-rest-server/src/test/java/net/n2oapp/security/admin/TestApplication.java
+++ security-admin-rest-server/src/test/java/net/n2oapp/security/admin/TestApplication.java
@@ -4,39 +4,16 @@ import net.n2oapp.platform.jaxrs.autoconfigure.EnableJaxRsProxyClient;
 import net.n2oapp.security.admin.rest.api.PermissionRestService;
 import net.n2oapp.security.admin.rest.api.RoleRestService;
 import net.n2oapp.security.admin.rest.api.UserRestService;
-import org.apache.cxf.Bus;
-import org.apache.cxf.common.util.ClasspathScanner;
-import org.apache.cxf.jaxrs.client.Client;
-import org.apache.cxf.jaxrs.client.spring.JaxRsProxyClientConfiguration;
-import org.apache.cxf.service.factory.ServiceConstructionException;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.util.ReflectionUtils;
-
-import javax.ws.rs.Path;
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.Proxy;
-import java.util.Collection;
-import java.util.Map;
-
-import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
 
 /**
  * Стартовая точка запуска Spring Boot
  */
 @SpringBootApplication
-@EnableJaxRsProxyClient({UserRestService.class, RoleRestService.class, PermissionRestService.class})
+@EnableJaxRsProxyClient(
+        classes = {UserRestService.class, RoleRestService.class, PermissionRestService.class},
+        address = "http://localhost:${server.port}/rest")
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
