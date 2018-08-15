b30572384267450a60b39dd102019655a6ed0713
rgalina <rgalina@i-novus.ru>
2018-08-15 10:50:09 +0300

    [nf] перевод админки на версию 7.0 попытка 2

--- security-admin-impl/src/test/java/net/n2oapp/security/admin/TestApplication.java
+++ security-admin-impl/src/test/java/net/n2oapp/security/admin/TestApplication.java
@@ -1,9 +1,7 @@
 package net.n2oapp.security.admin;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
