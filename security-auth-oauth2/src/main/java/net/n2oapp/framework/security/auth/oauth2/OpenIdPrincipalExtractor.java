0a63cfcbb63b493813dde25eb11f9a98002949e0
estartcev <estartcev@i-novus.ru>
2019-01-21 16:14:17 +0300

    [DOMRFLKB-381] добавлена авторизация по ролям

--- security-auth-oauth2/src/main/java/net/n2oapp/framework/security/auth/oauth2/OpenIdPrincipalExtractor.java
+++ security-auth-oauth2/src/main/java/net/n2oapp/framework/security/auth/oauth2/OpenIdPrincipalExtractor.java
@@ -5,20 +5,21 @@ import net.n2oapp.security.admin.api.service.UserDetailsService;
 import net.n2oapp.security.auth.User;
 import net.n2oapp.security.auth.authority.PermissionGrantedAuthority;
 import net.n2oapp.security.auth.authority.RoleGrantedAuthority;
+import org.apache.commons.collections.map.UnmodifiableMap;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Создание объекта пользователя из информации в SSO сервере
  */
-public class OpenIdPrincipalExtractor implements PrincipalExtractor {
+public class OpenIdPrincipalExtractor implements PrincipalExtractor, AuthoritiesExtractor {
+
+    private static final String GRANTED_AUTHORITY_KEY = "GrantedAuthorityKey";
+
     private static final String[] PRINCIPAL_KEYS = new String[]{"user", "username", "preferred_username",
             "login", "userid", "user_id", "id", "name", "sub"};
     private static final String[] SURNAME_KEYS = new String[]{"surname", "secondname", "second_name", "family_name"};
@@ -35,6 +36,46 @@ public class OpenIdPrincipalExtractor implements PrincipalExtractor {
 
     @Override
     public Object extractPrincipal(Map<String, Object> map) {
+        net.n2oapp.security.admin.api.model.User user = getUser(map);
+        if (user == null) {
+            return null;
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if (user.getRoles() != null) {
+            authorities.addAll(user.getRoles().stream().map(r -> new RoleGrantedAuthority(r.getCode())).collect(Collectors.toList()));
+            authorities.addAll(user.getRoles().stream().filter(r -> r.getPermissions() != null).flatMap(r -> r.getPermissions().stream())
+                    .map(p -> new PermissionGrantedAuthority(p.getCode())).collect(Collectors.toList()));
+            if(!(map instanceof UnmodifiableMap)) {
+                try {
+                    map.put(GRANTED_AUTHORITY_KEY, authorities);
+                } catch (Exception e) {
+                    //do nothing
+                }
+            }
+        }
+        return new User(user.getUsername(), "N/A", authorities, user.getSurname(), user.getName(),
+                user.getPatronymic(), user.getEmail());
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<GrantedAuthority> extractAuthorities(Map<String, Object> map) {
+        if (map.containsKey(GRANTED_AUTHORITY_KEY)) {
+            return (List<GrantedAuthority>) map.get(GRANTED_AUTHORITY_KEY);
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        net.n2oapp.security.admin.api.model.User user = getUser(map);
+        if (user != null && user.getRoles() != null) {
+            authorities.addAll(user.getRoles().stream().map(r -> new RoleGrantedAuthority(r.getCode())).collect(Collectors.toList()));
+            authorities.addAll(user.getRoles().stream().filter(r -> r.getPermissions() != null).flatMap(r -> r.getPermissions().stream())
+                    .map(p -> new PermissionGrantedAuthority(p.getCode())).collect(Collectors.toList()));
+        }
+        return authorities;
+    }
+
+    private net.n2oapp.security.admin.api.model.User getUser(Map<String, Object> map) {
         Object usernameObj = extractFromMap(PRINCIPAL_KEYS, map);
         if (usernameObj == null)
             return null;
@@ -55,16 +96,7 @@ public class OpenIdPrincipalExtractor implements PrincipalExtractor {
         token.setName(name);
         token.setEmail(email);
 
-        net.n2oapp.security.admin.api.model.User user = userDetailsService.loadUserDetails(token);
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        if (user.getRoles() != null) {
-            authorities.addAll(user.getRoles().stream().map(r -> new RoleGrantedAuthority(r.getCode())).collect(Collectors.toList()));
-            authorities.addAll(user.getRoles().stream().filter(r -> r.getPermissions() != null).flatMap(r -> r.getPermissions().stream())
-                    .map(p -> new PermissionGrantedAuthority(p.getCode())).collect(Collectors.toList()));
-        }
-        UserDetails userDetails = new User(username, "N/A", authorities, user.getSurname(), user.getName(),
-                user.getPatronymic(), user.getEmail());
-        return userDetails;
+        return userDetailsService.loadUserDetails(token);
     }
 
     private Object extractFromMap(String[] keys, Map<String, Object> map) {
