3a2c7647b3fb9088ad660ded0973e4450ecfba1b
iryabov <iryabov@i-novus.ru>
2019-01-29 15:56:33 +0300

    [NF] n2o 7.0.3 [NF] Сообщения об ошибках из rest

--- security-admin-web/src/main/java/net/n2oapp/security/admin/web/WebConfiguration.java
+++ security-admin-web/src/main/java/net/n2oapp/security/admin/web/WebConfiguration.java
@@ -1,5 +1,7 @@
 package net.n2oapp.security.admin.web;
 
+import net.n2oapp.framework.access.data.SecurityProvider;
+import net.n2oapp.framework.access.simple.PermissionApi;
 import net.n2oapp.framework.api.context.ContextProcessor;
 import net.n2oapp.framework.api.data.DomainProcessor;
 import net.n2oapp.framework.api.data.QueryProcessor;
@@ -7,15 +9,22 @@ import net.n2oapp.framework.engine.data.N2oInvocationFactory;
 import net.n2oapp.framework.engine.data.N2oQueryProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(ExceptionHandlerConfiguration.class)
 public class WebConfiguration {
     @Bean
     public QueryProcessor queryProcessor(ContextProcessor contextProcessor,
                                          DomainProcessor domainProcessor,
                                          N2oInvocationFactory invocationFactory) {
-        N2oQueryProcessor queryProcessor = new N2oQueryProcessor(contextProcessor, invocationFactory);
+        N2oQueryProcessor queryProcessor = new N2oQueryProcessor(invocationFactory, contextProcessor, domainProcessor);
         queryProcessor.setCriteriaResolver(new BaseCriteriaConstructor());
         return queryProcessor;
+    }
+
+    @Bean //todo убрать в 7.0.4
+    public SecurityProvider securityProvider(PermissionApi permissionApi) {
+        return new SecurityProvider(permissionApi);
     }
 }
