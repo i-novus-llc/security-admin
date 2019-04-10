9c7f5a079a488fe23b31be82799a336f222051fb
estartcev <estartcev@i-novus.ru>
2019-04-10 14:07:07 +0300

    [MICROSERVICES-133] Добавлена отправка токена клиенту

--- security-auth-oauth2/src/main/java/net/n2oapp/framework/security/auth/oauth2/OpenIdSecurityConfigurerAdapter.java
+++ security-auth-oauth2/src/main/java/net/n2oapp/framework/security/auth/oauth2/OpenIdSecurityConfigurerAdapter.java
@@ -2,6 +2,7 @@ package net.n2oapp.framework.security.auth.oauth2;
 
 import net.n2oapp.security.admin.api.service.UserDetailsService;
 import net.n2oapp.security.auth.N2oSecurityConfigurerAdapter;
+import net.n2oapp.security.auth.context.SpringSecurityUserContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
@@ -9,6 +10,10 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoPr
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 
 /**
  * Адаптер для настройки SSO аутентификации по протоколу OAuth2 OpenId Connect
@@ -34,8 +39,37 @@ public abstract class OpenIdSecurityConfigurerAdapter extends N2oSecurityConfigu
         return new OpenIdPrincipalExtractor(userDetailsService);
     }
 
+    @Override
+    public SpringSecurityUserContext springSecurityUserContext() {
+        return new SpringSecurityUserContext() {
+            @Override
+            public Object get(String param) {
+                if ("token".equals(param)) {
+                    OAuth2AuthenticationDetails details = getAuthenticationDetails();
+                    if (details != null) {
+                        return details.getTokenValue();
+                    }
+                }
+                return super.get(param);
+            }
+        };
+    }
 
     protected LogoutConfigurer<HttpSecurity> configureLogout(LogoutConfigurer<HttpSecurity> logout) throws Exception {
         return logout.logoutSuccessUrl(ssoLogout != null ? ssoLogout : "/");
+    }
+
+    private OAuth2AuthenticationDetails getAuthenticationDetails() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        if (context != null) {
+            Authentication authentication = context.getAuthentication();
+            if (authentication != null) {
+                Object details = authentication.getDetails();
+                if (details instanceof OAuth2AuthenticationDetails) {
+                    return (OAuth2AuthenticationDetails) details;
+                }
+            }
+        }
+        return null;
     }
 }
