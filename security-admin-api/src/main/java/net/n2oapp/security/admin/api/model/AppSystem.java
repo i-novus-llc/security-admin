c6f873094b3dc88c45611578d470c5d0db2a9558
iryabov <iryabov@i-novus.ru>
2019-09-02 17:46:59 +0300

    DOMRFLKB-3723 Поддержка обратной совместимости в моделях

--- security-admin-api/src/main/java/net/n2oapp/security/admin/api/model/AppSystem.java
+++ security-admin-api/src/main/java/net/n2oapp/security/admin/api/model/AppSystem.java
@@ -1,5 +1,6 @@
 package net.n2oapp.security.admin.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -14,6 +15,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @ApiModel("Система")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AppSystem {
 
     @JsonProperty
