4398820aa5a3ac1913b3421b218c90f9695b9691
btsyganov <btsyganov@i-novus.ru>
2019-09-24 16:44:10 +0300

    [nf][EPMP-334] сервис экспорта систем и приложений в НСИ

--- security-admin-api/src/main/java/net/n2oapp/security/admin/api/criteria/SystemCriteria.java
+++ security-admin-api/src/main/java/net/n2oapp/security/admin/api/criteria/SystemCriteria.java
@@ -1,12 +1,18 @@
 package net.n2oapp.security.admin.api.criteria;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Критерий фильтрации систем
  */
 @Data
+@NoArgsConstructor
 public class SystemCriteria extends BaseCriteria {
     private String code;
     private String name;
+
+    public SystemCriteria(int page, int size) {
+        super(page, size);
+    }
 }
