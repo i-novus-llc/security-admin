58294c16682a7c7601873e3703007034b020a646
estartcev <estartcev@i-novus.ru>
2019-09-26 14:29:05 +0300

    [EPMP-51] Добавлен аудит

---
+++ security-admin-backend/src/main/java/net/n2oapp/security/admin/AdminBackendConfiguration.java
@@ -0,0 +1,27 @@
+package net.n2oapp.security.admin;
+
+import net.n2oapp.platform.jaxrs.autoconfigure.EnableJaxRsProxyClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.i_novus.ms.audit.client.AuditClient;
+import ru.i_novus.ms.audit.client.impl.SimpleAuditClientImpl;
+import ru.i_novus.ms.audit.service.api.AuditRest;
+
+@Configuration
+public class AdminBackendConfiguration {
+
+    @Configuration
+    @EnableJaxRsProxyClient(
+            classes = {AuditRest.class},
+            address = "${audit.service.url}")
+    static class AuditClientConfiguration {
+        @Bean
+        public AuditClient simpleAuditClient(@Qualifier("auditRestJaxRsProxyClient") AuditRest auditRest) {
+            SimpleAuditClientImpl simpleAuditClient = new SimpleAuditClientImpl();
+            simpleAuditClient.setAuditRest(auditRest);
+            return simpleAuditClient;
+        }
+    }
+
+}
--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/AdminImplConfiguration.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/AdminImplConfiguration.java
@@ -1,14 +1,13 @@
 package net.n2oapp.security.admin.impl;
 
-import net.n2oapp.platform.jaxrs.autoconfigure.EnableJaxRsProxyClient;
 import net.n2oapp.security.admin.api.provider.SsoUserRoleProvider;
 import net.n2oapp.security.admin.api.service.UserService;
 import net.n2oapp.security.admin.commons.AdminCommonsConfiguration;
+import net.n2oapp.security.admin.commons.util.SecurityContextUtils;
 import net.n2oapp.security.admin.impl.provider.SimpleSsoUserRoleProvider;
 import net.n2oapp.security.admin.impl.repository.RoleRepository;
 import net.n2oapp.security.admin.impl.repository.UserRepository;
 import net.n2oapp.security.admin.impl.service.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,11 +15,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ru.i_novus.ms.audit.client.AuditClient;
-import ru.i_novus.ms.audit.client.impl.SimpleAuditClientImpl;
 import ru.i_novus.ms.audit.client.impl.converter.RequestConverter;
 import ru.i_novus.ms.audit.client.model.User;
-import ru.i_novus.ms.audit.service.api.AuditRest;
 
 
 @Configuration
@@ -30,7 +26,6 @@ import ru.i_novus.ms.audit.service.api.AuditRest;
 @ComponentScan({"net.n2oapp.security.admin.impl", "net.n2oapp.security.admin.api"})
 @Import(AdminCommonsConfiguration.class)
 public class AdminImplConfiguration {
-
 
     @Bean
     public UserService userService(UserRepository userRepository, RoleRepository roleRepository, SsoUserRoleProvider ssoUserRoleProvider) {
@@ -42,28 +37,17 @@ public class AdminImplConfiguration {
         return new SimpleSsoUserRoleProvider();
     }
 
-
     @Bean
     public RequestConverter requestConverter() {
         return new RequestConverter(
-                () -> new User("UNKNOWN", "UNKNOWN"),   //FIXME
+                this::getUser,
                 () -> "Access",
-                () -> "SOURCE_WORKSTATION"
+                SecurityContextUtils::getRequestHost
         );
     }
 
-    @Configuration
-    @EnableJaxRsProxyClient(
-            classes = {AuditRest.class},
-            address = "${audit.rest.url}")  //TODO добавить
-    static class AuditClientConfiguration {
-        @Bean
-        public AuditClient simpleAuditClient(@Qualifier("auditRestJaxRsProxyClient") AuditRest auditRest) {
-            SimpleAuditClientImpl simpleAuditClient = new SimpleAuditClientImpl();
-            simpleAuditClient.setAuditRest(auditRest);
-            return simpleAuditClient;
-        }
+    private User getUser() {
+        return new User("UNKNOWN", SecurityContextUtils.getJwtTokenValue("email"));
     }
-
 
 }
