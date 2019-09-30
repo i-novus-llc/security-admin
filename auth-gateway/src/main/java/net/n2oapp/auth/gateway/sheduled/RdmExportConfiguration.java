7eb2c4d3863d73d4f0119950ed666df8cc1cd3b3
btsyganov <btsyganov@i-novus.ru>
2019-09-30 11:09:03 +0300

    [rf][EPMP-334] переконфигурировал для использования нескольких джобов

--- auth-gateway/src/main/java/net/n2oapp/auth/gateway/sheduled/RdmExportConfiguration.java
+++ auth-gateway/src/main/java/net/n2oapp/auth/gateway/sheduled/RdmExportConfiguration.java
@@ -1,14 +1,13 @@
 package net.n2oapp.auth.gateway.sheduled;
 
-import net.n2oapp.security.admin.api.service.ApplicationSystemExportService;
 import org.quartz.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import java.util.Map;
+import java.util.Set;
 
 @Configuration
 public class RdmExportConfiguration {
@@ -16,31 +15,29 @@ public class RdmExportConfiguration {
     @Value("${rdm.export.cron}")
     private String cronExpression;
 
-    @Autowired
-    private ApplicationSystemExportService exportService;
-
     @Bean
-    public JobDetail jobDetail() {
-        return JobBuilder.newJob().ofType(ApplicationSystemExportJob.class)
+    public Map<JobDetail, Set<? extends Trigger>> scheduleJobs() {
+        JobDetail jobDetail = JobBuilder.newJob().ofType(ApplicationSystemExportJob.class)
                 .storeDurably()
                 .withIdentity("app_sys_export_job")
                 .withDescription("Export Applications and Systems")
                 .usingJobData(new JobDataMap())
                 .build();
-    }
 
-    @Bean
-    public Trigger trigger(JobDetail job) {
-        return TriggerBuilder.newTrigger().forJob(job)
+        Trigger trigger = TriggerBuilder.newTrigger().forJob(jobDetail)
                 .withIdentity("app_sys_export_trigger")
                 .withDescription("Trigger for app_sys_export_job")
                 .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression))
                 .build();
+
+        return Map.of(jobDetail, Set.of(trigger));
     }
 
     @Bean
-    public SchedulerFactoryBeanCustomizer schedulerContextCustomizer() {
-        return (schedulerFactoryBean) ->
-                schedulerFactoryBean.setSchedulerContextAsMap(Map.of(ApplicationSystemExportService.class.getPackageName(), exportService));
+    public Scheduler scheduler(SchedulerFactoryBean schedulerFactoryBean, Map<JobDetail, Set<? extends Trigger>> scheduleJobs) throws SchedulerException {
+        Scheduler scheduler = schedulerFactoryBean.getScheduler();
+        scheduler.scheduleJobs(scheduleJobs, true);
+        return scheduler;
     }
+
 }
