b6a97e7e8f545a23bf7cf216ebda44df2838adec
estartcev <estartcev@i-novus.ru>
2019-10-01 14:02:04 +0300

    [EPMP-51] Сообщения вынесены из кода, восстановлены тесты

--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/AdminImplConfiguration.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/AdminImplConfiguration.java
@@ -5,18 +5,22 @@ import net.n2oapp.platform.jaxrs.autoconfigure.EnableJaxRsProxyClient;
 import net.n2oapp.security.admin.api.provider.SsoUserRoleProvider;
 import net.n2oapp.security.admin.api.service.UserService;
 import net.n2oapp.security.admin.commons.AdminCommonsConfiguration;
-import net.n2oapp.security.admin.commons.util.SecurityContextUtils;
 import net.n2oapp.security.admin.impl.provider.SimpleSsoUserRoleProvider;
 import net.n2oapp.security.admin.impl.repository.RoleRepository;
 import net.n2oapp.security.admin.impl.repository.UserRepository;
 import net.n2oapp.security.admin.impl.service.UserServiceImpl;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import ru.i_novus.ms.audit.client.impl.converter.RequestConverter;
-import ru.i_novus.ms.audit.client.model.User;
 
 import java.util.Locale;
 import ru.inovus.ms.rdm.provider.RdmMapperConfigurer;
@@ -68,11 +72,22 @@ public class AdminImplConfiguration {
         return new RequestConverter(
                 this::getUser,
                 () -> "Access",
-                SecurityContextUtils::getRequestHost
+                this::getRequestHost
         );
     }
 
-    private User getUser() {
-        return new User("UNKNOWN", SecurityContextUtils.getJwtTokenValue("email"));
+    private ru.i_novus.ms.audit.client.model.User getUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        net.n2oapp.security.auth.common.User user = (net.n2oapp.security.auth.common.User)auth.getPrincipal();
+        return new ru.i_novus.ms.audit.client.model.User("UNKNOWN", user.getEmail());
+    }
+
+    private String getRequestHost() {
+        try {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            return attributes.getRequest().getHeader(HttpHeaders.HOST);
+        } catch (Exception e) {
+            return "UNKNOWN";
+        }
     }
 }
