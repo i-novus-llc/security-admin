d4570948b4da12f94b0c05faac01d01736633bed
estartcev <estartcev@i-novus.ru>
2019-10-04 11:51:33 +0300

    [EPMP-131] Исправлены замечания МР

--- security-admin-sso-keycloak/src/main/java/net/n2oapp/security/admin/sso/keycloak/synchronization/UserSynchronizeJob.java
+++ security-admin-sso-keycloak/src/main/java/net/n2oapp/security/admin/sso/keycloak/synchronization/UserSynchronizeJob.java
@@ -3,24 +3,19 @@ package net.n2oapp.security.admin.sso.keycloak.synchronization;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Автоматический запуск синхронизации пользователей
  */
 public class UserSynchronizeJob implements Job {
 
+    @Autowired
+    private KeycloakUserSynchronizeProvider keycloakUserSynchronizeProvider;
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        getKeycloakUserSynchronizeProvider(context).startSynchronization();
+        keycloakUserSynchronizeProvider.startSynchronization();
     }
 
-    private KeycloakUserSynchronizeProvider getKeycloakUserSynchronizeProvider(JobExecutionContext context) {
-        String key = KeycloakUserSynchronizeProvider.class.getSimpleName();
-        try {
-            return (KeycloakUserSynchronizeProvider) context.getScheduler().getContext().get(key);
-        } catch (SchedulerException e) {
-            throw new IllegalStateException(e);
-        }
-    }
 }
