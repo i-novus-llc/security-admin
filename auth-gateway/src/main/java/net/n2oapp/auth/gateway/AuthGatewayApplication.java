7e01bd65dbdc0541c816fdf339927f190fae7b3c
btsyganov <btsyganov@i-novus.ru>
2019-10-07 16:31:58 +0300

    [bf][EPMP-422] импорт кейклок провайдера

--- auth-gateway/src/main/java/net/n2oapp/auth/gateway/AuthGatewayApplication.java
+++ auth-gateway/src/main/java/net/n2oapp/auth/gateway/AuthGatewayApplication.java
@@ -2,12 +2,13 @@ package net.n2oapp.auth.gateway;
 
 import net.n2oapp.security.admin.impl.AdminImplConfiguration;
 import net.n2oapp.security.admin.rest.impl.AdminRestServerConfiguration;
+import net.n2oapp.security.admin.sso.keycloak.SsoKeycloakConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({AdminRestServerConfiguration.class, AdminImplConfiguration.class})
+@Import({AdminRestServerConfiguration.class, AdminImplConfiguration.class, SsoKeycloakConfiguration.class})
 public class AuthGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthGatewayApplication.class, args);
