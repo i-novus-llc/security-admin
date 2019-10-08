7e37bb6f116a387472558c9d4739461d88090ba6
aarifullin <aarifullin@i-novus.ru>
2019-10-08 16:30:22 +0300

    [NF][EPMP-430] поправил скрипт для h2

--- security-admin-db/src/main/resources/security/admin/db/changelog/sql/V14_update_user_add_snils.sql
+++ security-admin-db/src/main/resources/security/admin/db/changelog/sql/V14_update_user_add_snils.sql
@@ -1,3 +1,3 @@
-ALTER TABLE sec."user" ADD COLUMN snils VARCHAR(14);
+ALTER TABLE sec.user ADD COLUMN snils VARCHAR(14);
 
-COMMENT ON COLUMN sec."user".snils IS 'СНИЛС пользователя';
+COMMENT ON COLUMN sec.user.snils IS 'СНИЛС пользователя';
