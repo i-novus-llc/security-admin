4359a780ccb06a922b3b8b5f1737f1a6d9398eb6
aarifullin <aarifullin@i-novus.ru>
2019-11-28 17:00:08 +0300

    [BF][SECURITY-942] query параметр сделал примитив

--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/service/UserLevelServiceImpl.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/service/UserLevelServiceImpl.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.Objects.isNull;
+
 /**
  * Сервис уровней пользователя
  */
@@ -28,6 +30,8 @@ public class UserLevelServiceImpl implements UserLevelService {
 
     @Override
     public List<UserLevel> getAllForFilter(String name) {
+        if (isNull(name))
+            return userLevelsForFilter;
         List<UserLevel> result = new ArrayList<>();
         result.addAll(userLevelsForFilter.stream().filter(userLevel -> userLevel.getDesc().toLowerCase().contains(name.toLowerCase())).collect(Collectors.toList()));
         return result;
