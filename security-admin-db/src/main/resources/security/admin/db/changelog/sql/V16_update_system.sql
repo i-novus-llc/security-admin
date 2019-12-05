edd3b1df743e6216e3b8b637ce6db0e2b00c1062
agolodnaya <agolodnaya@i-novus.ru>
2019-12-05 18:38:39 +0300

    [nf] Настройки представления системы в UI (FFOMSEPPD-155)

--- security-admin-db/src/main/resources/security/admin/db/changelog/sql/V16_update_system.sql
+++ security-admin-db/src/main/resources/security/admin/db/changelog/sql/V16_update_system.sql
@@ -1,14 +1,14 @@
-ALTER TABLE ${n2o.security.admin.schema}.system ADD COLUMN short_name VARCHAR,
+ALTER TABLE sec.system ADD COLUMN short_name VARCHAR,
 ADD COLUMN icon VARCHAR,
 ADD COLUMN url VARCHAR,
 ADD COLUMN public_access BOOLEAN,
 ADD COLUMN view_order INTEGER;
 
-COMMENT ON COLUMN ${n2o.security.admin.schema}.system.short_name IS 'Краткое наименование';
-COMMENT ON COLUMN ${n2o.security.admin.schema}.system.short_name IS 'Иконка';
-COMMENT ON COLUMN ${n2o.security.admin.schema}.system.short_name IS 'Адрес подсистемы';
-COMMENT ON COLUMN ${n2o.security.admin.schema}.system.short_name IS 'Признак работы в режиме без авторизации';
-COMMENT ON COLUMN ${n2o.security.admin.schema}.system.short_name IS 'Порядок отображения подсистемы';
+COMMENT ON COLUMN sec.system.short_name IS 'Краткое наименование';
+COMMENT ON COLUMN sec.system.short_name IS 'Иконка';
+COMMENT ON COLUMN sec.system.short_name IS 'Адрес подсистемы';
+COMMENT ON COLUMN sec.system.short_name IS 'Признак работы в режиме без авторизации';
+COMMENT ON COLUMN sec.system.short_name IS 'Порядок отображения подсистемы';
 
 
 
