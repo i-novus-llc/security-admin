989fac05725cb5be249dd1bbbd97b1a09e322122
agolodnaya <agolodnaya@i-novus.ru>
2019-12-06 11:19:14 +0300

    [cf] Настройки представления системы в UI (FFOMSEPPD-101)

--- security-admin-db/src/main/resources/security/admin/db/changelog/sql/V16_update_system.sql
+++ security-admin-db/src/main/resources/security/admin/db/changelog/sql/V16_update_system.sql
@@ -1,8 +1,8 @@
-ALTER TABLE sec.system ADD COLUMN short_name VARCHAR,
-ADD COLUMN icon VARCHAR,
-ADD COLUMN url VARCHAR,
-ADD COLUMN public_access BOOLEAN,
-ADD COLUMN view_order INTEGER;
+ALTER TABLE sec.system ADD COLUMN short_name VARCHAR;
+ALTER TABLE sec.system ADD COLUMN icon VARCHAR;
+ALTER TABLE sec.system ADD COLUMN url VARCHAR;
+ALTER TABLE sec.system ADD COLUMN public_access VARCHAR;
+ALTER TABLE sec.system ADD COLUMN view_order VARCHAR;
 
 COMMENT ON COLUMN sec.system.short_name IS 'Краткое наименование';
 COMMENT ON COLUMN sec.system.short_name IS 'Иконка';
