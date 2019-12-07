5f7dafaabc90d416ef77d7bcbbbcbf97109b7e09
Старцев Егор <estartcev@i-novus.ru>
2019-12-07 12:45:44 +0300

    Resolve SECURITY-61 "Story/"

--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/audit/AuditHelper.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/audit/AuditHelper.java
@@ -2,6 +2,8 @@ package net.n2oapp.security.admin.impl.audit;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceAccessor;
 import ru.i_novus.ms.audit.client.AuditClient;
@@ -13,14 +15,17 @@ import ru.i_novus.ms.audit.client.model.AuditClientRequest;
 
 public class AuditHelper {
 
-    @Autowired
-    private AuditClient auditClient;
-    @Autowired
-    private MessageSourceAccessor messageSourceAccessor;
+    private static final Logger log = LoggerFactory.getLogger(AuditHelper.class);
 
-    private ObjectMapper mapper = new ObjectMapper();
+    private AuditClient auditClient;
+    private MessageSourceAccessor messageSourceAccessor;
+    private ObjectMapper mapper;
 
     public void audit(String action, Object object, String objectId, String objectName) {
+        if (auditClient == null) {
+            log.warn("Audit service is disabled, please set 'audit.client.enabled' and 'audit.service.url' properties.");
+            return;
+        }
         AuditClientRequest request = new AuditClientRequest();
         request.setEventType(messageSourceAccessor.getMessage(action));
         request.setObjectType(object.getClass().getSimpleName());
@@ -36,4 +41,18 @@ public class AuditHelper {
         auditClient.add(request);
     }
 
+    @Autowired(required = false)
+    public void setAuditClient(AuditClient auditClient) {
+        this.auditClient = auditClient;
+    }
+
+    @Autowired
+    public void setMessageSourceAccessor(MessageSourceAccessor messageSourceAccessor) {
+        this.messageSourceAccessor = messageSourceAccessor;
+    }
+
+    @Autowired
+    public void setMapper(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
 }
