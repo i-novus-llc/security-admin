66935ff203fad4e255056a11eab8266ac3254f04
aarifullin <aarifullin@i-novus.ru>
2020-01-30 11:56:06 +0300

    [NF][SECURITY-28] сломал чексумму ченжсета

--- security-admin-db/src/main/resources/security/admin/db/changelog/sql/V16_update_system.sql
+++ security-admin-db/src/main/resources/security/admin/db/changelog/sql/V16_update_system.sql
@@ -3,14 +3,12 @@ ALTER TABLE sec.system ADD COLUMN icon VARCHAR;
 ALTER TABLE sec.system ADD COLUMN url VARCHAR;
 ALTER TABLE sec.system ADD COLUMN public_access VARCHAR;
 ALTER TABLE sec.system ADD COLUMN view_order VARCHAR;
-ALTER TABLE sec.system ADD COLUMN show_on_interface boolean;
 
 COMMENT ON COLUMN sec.system.short_name IS 'Краткое наименование';
 COMMENT ON COLUMN sec.system.short_name IS 'Иконка';
 COMMENT ON COLUMN sec.system.short_name IS 'Адрес подсистемы';
 COMMENT ON COLUMN sec.system.short_name IS 'Признак работы в режиме без авторизации';
 COMMENT ON COLUMN sec.system.short_name IS 'Порядок отображения подсистемы';
-COMMENT ON COLUMN sec.system.show_on_interface IS 'Отображение систем в едином интерфейсе';
 
 
 
