c8713b5b64e93669260a2b1e451d810c3a73ec34
aarifullin <aarifullin@i-novus.ru>
2020-02-04 13:01:41 +0300

    [BF][EPMP-1109] Появляется старая форма авторизации при переходе на /

--- security-admin-auth-server/src/main/java/net/n2oapp/security/admin/auth/server/OAuthServerConfiguration.java
+++ security-admin-auth-server/src/main/java/net/n2oapp/security/admin/auth/server/OAuthServerConfiguration.java
@@ -138,12 +138,18 @@ public class OAuthServerConfiguration extends OAuth2AuthorizationServerConfigura
          */
         @Controller
         public static class IndexController {
+
+            @Value("${access.auth.redirect:#{null}}")
+            String redirectTo;
+
             @RequestMapping("/")
             public ModelAndView index() {
                 ModelAndView mv = new ModelAndView();
                 SecurityContext sc = SecurityContextHolder.getContext();
                 if (sc != null && sc.getAuthentication() instanceof OAuth2Authentication)
                     mv.setViewName("forward:alreadyLogged.html");
+                else if (redirectTo != null && !redirectTo.isEmpty())
+                    mv.setViewName("redirect:" + redirectTo);
                 else
                     mv.setViewName("forward:index.html");
                 return mv;
