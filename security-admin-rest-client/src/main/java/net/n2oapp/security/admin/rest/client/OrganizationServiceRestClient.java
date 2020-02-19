f55c6caa9ed03c7cef43c2c514d515b09b1ad884
btsyganov <btsyganov@i-novus.ru>
2020-02-19 12:52:13 +0300

    [bf][SECURITY-100] fix rest client

--- security-admin-rest-client/src/main/java/net/n2oapp/security/admin/rest/client/OrganizationServiceRestClient.java
+++ security-admin-rest-client/src/main/java/net/n2oapp/security/admin/rest/client/OrganizationServiceRestClient.java
@@ -1,9 +1,12 @@
 package net.n2oapp.security.admin.rest.client;
 
+import net.n2oapp.security.admin.api.criteria.OrgCategoryCriteria;
 import net.n2oapp.security.admin.api.criteria.OrganizationCriteria;
+import net.n2oapp.security.admin.api.model.OrgCategory;
 import net.n2oapp.security.admin.api.model.Organization;
 import net.n2oapp.security.admin.api.service.OrganizationService;
 import net.n2oapp.security.admin.rest.api.OrganizationRestService;
+import net.n2oapp.security.admin.rest.api.criteria.RestOrgCategoryCriteria;
 import net.n2oapp.security.admin.rest.api.criteria.RestOrganizationCriteria;
 import org.springframework.data.domain.Page;
 
@@ -26,6 +29,16 @@ public class OrganizationServiceRestClient implements OrganizationService {
         restOrganizationCriteria.setOrders(criteria.getOrders());
         restOrganizationCriteria.setSize(criteria.getSize());
         return client.getAll(restOrganizationCriteria);
+    }
+
+    @Override
+    public Page<OrgCategory> findAllCategories(OrgCategoryCriteria criteria) {
+        RestOrgCategoryCriteria restCriteria = new RestOrgCategoryCriteria();
+        restCriteria.setName(criteria.getName());
+        restCriteria.setSize(criteria.getSize());
+        restCriteria.setPage(criteria.getPage());
+        restCriteria.setOrders(criteria.getOrders());
+        return client.getAllCategories(restCriteria);
     }
 
 }
