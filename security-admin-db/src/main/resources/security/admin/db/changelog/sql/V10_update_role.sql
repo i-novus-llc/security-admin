04c0ff5eef185db68e092f55044d1e41d918a357
aarifullin <aarifullin@i-novus.ru>
2020-03-26 15:25:24 +0300

    [NF][EPMP-1191] удалил старый changelog, gateway переключил на новый

--- security-admin-db/src/main/resources/security/admin/db/changelog/sql/V10_update_role.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE ${n2o.security.admin.schema}.${n2o.security.admin.role.table} ADD COLUMN system_code VARCHAR(50);
-ALTER TABLE ${n2o.security.admin.schema}.${n2o.security.admin.role.table}
-    ADD CONSTRAINT role_system_code_fk FOREIGN KEY (system_code) REFERENCES ${n2o.security.admin.schema}.system (code);
-COMMENT ON COLUMN ${n2o.security.admin.schema}.${n2o.security.admin.role.table}.system_code IS 'Прикладная система (подсистема, модуль), которой принадлежит роль';
-
-ALTER TABLE ${n2o.security.admin.schema}.${n2o.security.admin.role.table} ADD COLUMN user_level VARCHAR(50);
-COMMENT ON COLUMN ${n2o.security.admin.schema}.${n2o.security.admin.role.table}.user_level IS 'Уровень пользователя, для которого предназначена роль';
--- security-admin-db/src/main/resources/security/admin/db/changelog/sql/V1_create_schema_sec.sql
+++
@@ -1,3 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS ${n2o.security.admin.schema};
-
-COMMENT ON SCHEMA ${n2o.security.admin.schema} IS 'Схема управления доступом';
