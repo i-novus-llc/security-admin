1cd78b1fb420556f4c369442170de7d4dda18895
jenkins <ci@i-novus.ru>
2020-07-10 10:48:32 +0300

    Automatically mirroring by Jenkins

--- security-admin-rest-client/src/main/java/net/n2oapp/security/admin/rest/client/OrganizationServiceRestClient.java
+++ security-admin-rest-client/src/main/java/net/n2oapp/security/admin/rest/client/OrganizationServiceRestClient.java
@@ -5,6 +5,7 @@ import net.n2oapp.security.admin.api.criteria.OrganizationCriteria;
 import net.n2oapp.security.admin.api.model.OrgCategory;
 import net.n2oapp.security.admin.api.model.Organization;
 import net.n2oapp.security.admin.api.service.OrganizationService;
+import net.n2oapp.security.admin.rest.api.OrganizationPersistRestService;
 import net.n2oapp.security.admin.rest.api.OrganizationRestService;
 import net.n2oapp.security.admin.rest.api.criteria.RestOrgCategoryCriteria;
 import net.n2oapp.security.admin.rest.api.criteria.RestOrganizationCriteria;
@@ -15,10 +16,13 @@ import org.springframework.data.domain.Page;
  */
 public class OrganizationServiceRestClient implements OrganizationService {
 
-    private OrganizationRestService client;
+    private OrganizationRestService organizationRestClient;
 
-    public OrganizationServiceRestClient(OrganizationRestService client) {
-        this.client = client;
+    private OrganizationPersistRestService organizationPersistRestClient;
+
+    public OrganizationServiceRestClient(OrganizationRestService organizationRestClient, OrganizationPersistRestService organizationPersistRestClient) {
+        this.organizationRestClient = organizationRestClient;
+        this.organizationPersistRestClient = organizationPersistRestClient;
     }
 
     @Override
@@ -33,7 +37,12 @@ public class OrganizationServiceRestClient implements OrganizationService {
         restOrganizationCriteria.setSystemCodes(criteria.getSystemCodes());
         restOrganizationCriteria.setInn(criteria.getInn());
         restOrganizationCriteria.setCategoryCodes(criteria.getCategoryCodes());
-        return client.getAll(restOrganizationCriteria);
+        return organizationRestClient.getAll(restOrganizationCriteria);
+    }
+
+    @Override
+    public Organization find(Integer id) {
+        return organizationRestClient.get(id);
     }
 
     @Override
@@ -43,7 +52,27 @@ public class OrganizationServiceRestClient implements OrganizationService {
         restCriteria.setSize(criteria.getSize());
         restCriteria.setPage(criteria.getPage());
         restCriteria.setOrders(criteria.getOrders());
-        return client.getAllCategories(restCriteria);
+        return organizationRestClient.getAllCategories(restCriteria);
     }
 
+    @Override
+    public Organization create(Organization organization) {
+        if (organizationPersistRestClient == null)
+            throw new UnsupportedOperationException();
+        return organizationPersistRestClient.create(organization);
+    }
+
+    @Override
+    public Organization update(Organization organization) {
+        if (organizationPersistRestClient == null)
+            throw new UnsupportedOperationException();
+        return organizationPersistRestClient.update(organization);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        if (organizationPersistRestClient == null)
+            throw new UnsupportedOperationException();
+        organizationPersistRestClient.delete(id);
+    }
 }
