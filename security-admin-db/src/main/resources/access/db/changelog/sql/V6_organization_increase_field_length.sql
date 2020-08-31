c794fe59811b622125c1197118e5a5152eb488c9
aarifullin <aarifullin@i-novus.ru>
2020-08-31 19:27:39 +0300

    [BF][RUSSPASS-268] увеличил длину полей в сущности organization

---
+++ security-admin-db/src/main/resources/access/db/changelog/sql/V6_organization_increase_field_length.sql
@@ -0,0 +1,3 @@
+ALTER TABLE sec.organization ALTER COLUMN code TYPE varchar(100);
+ALTER TABLE sec.organization ALTER COLUMN short_name TYPE varchar(500);
+ALTER TABLE sec.organization ALTER COLUMN full_name TYPE varchar(1000);
