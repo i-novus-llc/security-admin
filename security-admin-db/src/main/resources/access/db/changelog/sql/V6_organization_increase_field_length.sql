cdb48e62e92c1bac725ae73f36a0a1a267920861
aarifullin <aarifullin@i-novus.ru>
2020-09-01 15:08:31 +0300

    [BF][RUSSPASS-268] фикс

--- security-admin-db/src/main/resources/access/db/changelog/sql/V6_organization_increase_field_length.sql
+++ security-admin-db/src/main/resources/access/db/changelog/sql/V6_organization_increase_field_length.sql
@@ -1,5 +1,5 @@
 ALTER TABLE sec.organization ALTER COLUMN code TYPE text;
 ALTER TABLE sec.organization ALTER COLUMN short_name TYPE text;
 ALTER TABLE sec.organization ALTER COLUMN full_name TYPE text;
-ALTER TABLE sec.organization ALTER COLUMN legal_address text;
-ALTER TABLE sec.organization ALTER COLUMN email text;
+ALTER TABLE sec.organization ALTER COLUMN legal_address TYPE text;
+ALTER TABLE sec.organization ALTER COLUMN email TYPE text;
