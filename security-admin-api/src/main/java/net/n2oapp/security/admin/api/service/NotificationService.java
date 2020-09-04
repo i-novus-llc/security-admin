02c567cf2b4fe90b513468219af50ef759ad73de
btsyganov <btsyganov@i-novus.ru>
2020-09-04 14:08:13 +0300

    [rf][RUSSPASSIDM-56] убрал обращение к сервису уведомлений

--- security-admin-api/src/main/java/net/n2oapp/security/admin/api/service/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package net.n2oapp.security.admin.api.service;
-
-import net.n2oapp.security.admin.api.model.NotificationClientModel;
-
-public interface NotificationService {
-    Integer createClient(NotificationClientModel clientModel);
-}
