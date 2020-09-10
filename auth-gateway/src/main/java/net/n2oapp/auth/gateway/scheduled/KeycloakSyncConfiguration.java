4d8fed344511a0f1b801c41a849a2c0bc6cf223d
igilfanov <igilfanov@i-novus.ru>
2020-09-10 11:20:28 +0300

    cf SECURITY-211 fixed some Sonar's issues

--- auth-gateway/src/main/java/net/n2oapp/auth/gateway/scheduled/KeycloakSyncConfiguration.java
+++ auth-gateway/src/main/java/net/n2oapp/auth/gateway/scheduled/KeycloakSyncConfiguration.java
@@ -3,7 +3,6 @@ package net.n2oapp.auth.gateway.scheduled;
 import net.n2oapp.security.admin.sso.keycloak.AdminSsoKeycloakProperties;
 import net.n2oapp.security.admin.sso.keycloak.synchronization.UserSynchronizeJob;
 import org.quartz.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,12 +16,9 @@ public class KeycloakSyncConfiguration {
     public static final String USER_SYNCHRONIZE_JOB_DETAIL = "User_Synchronize_Job_Detail";
     private static final String USER_SYNCHRONIZE_TRIGGER = "User_Synchronize_Trigger";
 
-    @Autowired
-    private AdminSsoKeycloakProperties properties;
-
     @Bean
     @ConditionalOnProperty(name = "access.keycloak.synchronize-enabled")
-    public SynchronizationInfo userJobAndTrigger() {
+    public SynchronizationInfo userJobAndTrigger(AdminSsoKeycloakProperties properties) {
         JobDetail userSynchronizeJobDetail = JobBuilder.newJob().ofType(UserSynchronizeJob.class)
                 .storeDurably()
                 .withIdentity(USER_SYNCHRONIZE_JOB_DETAIL)
