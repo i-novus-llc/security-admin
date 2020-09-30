c30c7dae1d8a588db6103e19726530b1c1cf2edb
Молоствов Игорь <imolostvov@i-novus.ru>
2020-09-30 16:56:42 +0300

    [BF][SECURITY-253] Убрал дубликаты

--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/scheduled/DepartmentSynchronizeJob.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/scheduled/DepartmentSynchronizeJob.java
@@ -1,36 +1,23 @@
 package net.n2oapp.security.admin.impl.scheduled;
 
-import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import ru.inovus.ms.rdm.sync.rest.RdmSyncRest;
 
 @Component
-public class DepartmentSynchronizeJob implements Job {
+public class DepartmentSynchronizeJob extends SynchronizeJob {
 
     private static final Logger logger = LoggerFactory.getLogger(DepartmentSynchronizeJob.class);
 
     @Value("${rdm.sync.ref_book_code.department}")
     private String departmentRefBookCode;
 
-    private RdmSyncRest getRdmSyncRest(JobExecutionContext context) {
-        String key = RdmSyncRest.class.getSimpleName();
-        try {
-            return (RdmSyncRest) context.getScheduler().getContext().get(key);
-        } catch (SchedulerException e) {
-            logger.error("cannot get " + key + "property", e);
-            throw new RuntimeException(e);
-        }
-    }
-
     @Override
     public void execute(JobExecutionContext context) {
         logger.info("Department sync is started");
-        getRdmSyncRest(context).update(departmentRefBookCode);
+        getRdmSyncRest(context, logger).update(departmentRefBookCode);
         logger.info("Department sync is completed");
     }
 }
--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/scheduled/OrganizationSynchronizeJob.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/scheduled/OrganizationSynchronizeJob.java
@@ -1,38 +1,25 @@
 package net.n2oapp.security.admin.impl.scheduled;
 
-import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-import ru.inovus.ms.rdm.sync.rest.RdmSyncRest;
 
 @Component
 @ConditionalOnProperty(name = "access.organization-persist-mode", havingValue = "sync")
-public class OrganizationSynchronizeJob implements Job {
+public class OrganizationSynchronizeJob extends SynchronizeJob {
 
     private static final Logger logger = LoggerFactory.getLogger(OrganizationSynchronizeJob.class);
 
     @Value("${rdm.sync.ref_book_code.organization}")
     private String organizationRefBookCode;
 
-    private RdmSyncRest getRdmSyncRest(JobExecutionContext context) {
-        String key = RdmSyncRest.class.getSimpleName();
-        try {
-            return (RdmSyncRest) context.getScheduler().getContext().get(key);
-        } catch (SchedulerException e) {
-            logger.error("cannot get " + key + "property", e);
-            throw new RuntimeException(e);
-        }
-    }
-
     @Override
     public void execute(JobExecutionContext context) {
         logger.info("Organization sync is started");
-        getRdmSyncRest(context).update(organizationRefBookCode);
+        getRdmSyncRest(context, logger).update(organizationRefBookCode);
         logger.info("Organization sync is completed");
     }
 }
