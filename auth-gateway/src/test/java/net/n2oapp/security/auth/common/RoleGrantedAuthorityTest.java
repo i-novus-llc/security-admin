2eab86f2d38c3182b0bdbab0cd5a627febfeffb5
igilfanov <igilfanov@i-novus.ru>
2020-10-06 13:40:49 +0300

    cf SECURITY-265 fixed some Sonar's issues

--- auth-gateway/src/test/java/net/n2oapp/security/auth/common/RoleGrantedAuthorityTest.java
+++ auth-gateway/src/test/java/net/n2oapp/security/auth/common/RoleGrantedAuthorityTest.java
@@ -1,11 +1,13 @@
 package net.n2oapp.security.auth.common;
 
 import net.n2oapp.security.auth.common.authority.RoleGrantedAuthority;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static net.n2oapp.security.auth.common.TestConstants.OTHER_ROLE_NAME;
 import static net.n2oapp.security.auth.common.TestConstants.SOME_ROLE_NAME;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class RoleGrantedAuthorityTest {
 
@@ -14,13 +16,12 @@ public class RoleGrantedAuthorityTest {
     @Test
     public void testGetAuthority() {
         RoleGrantedAuthority auth = new RoleGrantedAuthority(SOME_ROLE_NAME);
-        Assert.assertEquals(auth.getAuthority(), DEFAULT_ROLE_PREFIX + SOME_ROLE_NAME);
+        assertEquals(DEFAULT_ROLE_PREFIX + SOME_ROLE_NAME, auth.getAuthority());
     }
 
     @Test
     public void testToString() {
-        RoleGrantedAuthority auth = new RoleGrantedAuthority(SOME_ROLE_NAME);
-        Assert.assertEquals(auth.toString(), SOME_ROLE_NAME);
+        assertEquals(SOME_ROLE_NAME, new RoleGrantedAuthority(SOME_ROLE_NAME).toString());
     }
 
     @Test
@@ -28,22 +29,22 @@ public class RoleGrantedAuthorityTest {
         RoleGrantedAuthority x = new RoleGrantedAuthority(SOME_ROLE_NAME);
         RoleGrantedAuthority y = new RoleGrantedAuthority(SOME_ROLE_NAME);
 
-        Assert.assertTrue(x.equals(x) && x.equals(x));
-        Assert.assertTrue(x.hashCode() == x.hashCode());
+        assertTrue(x.equals(x) && x.equals(x));
+        assertTrue(x.hashCode() == x.hashCode());
 
-        Assert.assertTrue(x.equals(y) && y.equals(x));
-        Assert.assertTrue(x.hashCode() == y.hashCode());
+        assertTrue(x.equals(y) && y.equals(x));
+        assertTrue(x.hashCode() == y.hashCode());
 
         RoleGrantedAuthority z = new RoleGrantedAuthority(OTHER_ROLE_NAME);
 
-        Assert.assertFalse(x.equals(z));
-        Assert.assertFalse(z.equals(x));
-        Assert.assertFalse(x.hashCode() == z.hashCode());
+        assertFalse(x.equals(z));
+        assertFalse(z.equals(x));
+        assertFalse(x.hashCode() == z.hashCode());
 
         Object otherObject = new Object();
 
-        Assert.assertFalse(x.equals(otherObject));
-        Assert.assertFalse(otherObject.equals(x));
-        Assert.assertFalse(x.hashCode() == otherObject.hashCode());
+        assertFalse(x.equals(otherObject));
+        assertFalse(otherObject.equals(x));
+        assertFalse(x.hashCode() == otherObject.hashCode());
     }
 }
