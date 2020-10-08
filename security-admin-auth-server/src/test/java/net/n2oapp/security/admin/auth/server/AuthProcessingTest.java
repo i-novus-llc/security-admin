1b357b70afc8fccf64af65e92f0c9d819c01bde9
btsyganov <btsyganov@i-novus.ru>
2020-10-08 16:04:17 +0300

    [nt][SECURITY-277] тест аутентификации

--- security-admin-auth-server/src/test/java/net/n2oapp/security/admin/auth/server/AuthProcessingTest.java
+++ security-admin-auth-server/src/test/java/net/n2oapp/security/admin/auth/server/AuthProcessingTest.java
@@ -2,9 +2,17 @@ package net.n2oapp.security.admin.auth.server;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.n2oapp.security.admin.api.model.Client;
+import net.n2oapp.security.admin.api.model.*;
 import net.n2oapp.security.admin.api.service.ClientService;
+import net.n2oapp.security.admin.impl.entity.PermissionEntity;
+import net.n2oapp.security.admin.impl.entity.RoleEntity;
+import net.n2oapp.security.admin.impl.entity.UserEntity;
 import net.n2oapp.security.admin.impl.repository.UserRepository;
+import net.n2oapp.security.admin.impl.service.UserDetailsServiceImpl;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +24,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -23,20 +32,30 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestPropertySource("classpath:test.properties")
 public class AuthProcessingTest {
 
     @LocalServerPort
     private String port;
+
+    private String host;
 
     @MockBean
     private ClientService clientService;
@@ -44,11 +63,24 @@ public class AuthProcessingTest {
     @MockBean
     private UserRepository userRepository;
 
-    RestTemplate client = new RestTemplate();
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    private static final RestTemplate client;
+
+    static {
+        client = new RestTemplate();
+        HttpComponentsClientHttpRequestFactory f = new HttpComponentsClientHttpRequestFactory();
+        f.setHttpClient(HttpClientBuilder.create().disableRedirectHandling().build());
+        client.setRequestFactory(f);
+    }
 
     @BeforeEach
     public void beforeEach() {
-        Mockito.when(clientService.findByClientId("test")).thenReturn(client());
+        host = "http://localhost:" + port;
+        when(clientService.findByClientId("test")).thenReturn(client());
+        when(userRepository.findOneByUsernameIgnoreCase("testUser")).thenReturn(userEntity());
+        when(userDetailsService.loadUserDetails(Mockito.any(UserDetailsToken.class))).thenReturn(user());
     }
 
 
@@ -65,7 +97,7 @@ public class AuthProcessingTest {
         grantType.add("grant_type", "client_credentials");
         HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(grantType, headers);
 
-        Map<String, Object> tokenResponse = client.exchange("http://localhost:" + port + "/oauth/token", HttpMethod.POST, entity, Map.class).getBody();
+        Map<String, Object> tokenResponse = client.exchange(host + "/oauth/token", HttpMethod.POST, entity, Map.class).getBody();
         assertThat(tokenResponse, notNullValue());
         assertThat(tokenResponse.get("token_type"), is("bearer"));
         assertThat(tokenResponse.get("jti"), notNullValue());
@@ -78,6 +110,75 @@ public class AuthProcessingTest {
         assertThat(claims.get("jti"), is(tokenResponse.get("jti")));
         assertThat(claims.get("client_id"), is("test"));
         assertThat((List<String>) claims.get("scope"), containsInAnyOrder("write", "read"));
+    }
+
+    @Test
+    public void testAuthorizationCode() throws IOException {
+
+        String url = host + "/oauth/authorize?client_id=test&redirect_uri=https://localhost:8080/login&response_type=code&scope=read%20write&state=T45FVY";
+        Response response = WebClient.create(url).get();
+        assertThat(response.getStatus(), is(302));
+        assertThat(response.getLocation().toString(), is(host + "/login/keycloak"));
+        NewCookie authSession = response.getCookies().get("JSESSIONID");
+
+        url = host + "/login/keycloak";
+        response = WebClient.create(url).cookie(authSession).get();
+
+        String state = extractQueryParameter(response.getLocation(), "state");
+
+        url = host + "/login/keycloak" +
+                "?state=" + state + "&session_state=testSessionState" +
+                "&code=testCode";
+
+        response = WebClient.create(url).cookie(authSession).get();
+        response = WebClient.create(response.getLocation()).cookie(authSession).get();
+        String code = extractQueryParameter(response.getLocation(), "code");
+
+        url = host + "/oauth/token";
+
+        Map<String, Object> tokenResponse = new ObjectMapper().readValue(
+                WebClient.create(url)
+                        .header("Authorization", "Basic dGVzdDp0ZXN0")
+                        .form(new Form()
+                                .param("grant_type", "authorization_code")
+                                .param("code", code)
+                                .param("redirect_uri", "https://localhost:8080/login")
+                        ).readEntity(String.class),
+                Map.class);
+
+        assertThat(tokenResponse.get("access_token"), notNullValue());
+        assertThat(tokenResponse.get("token_type"), is("bearer"));
+        assertThat(tokenResponse.get("expires_in"), notNullValue());
+        assertThat(tokenResponse.get("scope"), notNullValue());
+        assertThat(tokenResponse.get("jti"), notNullValue());
+
+        url = host + "/userinfo";
+
+        Map<String, Object> userInfo = new ObjectMapper().readValue(
+                WebClient.create(url)
+                        .header("Authorization", "Bearer " + tokenResponse.get("access_token"))
+                        .get()
+                        .readEntity(String.class), Map.class);
+
+        assertThat(userInfo.get("email"), is("testEmail"));
+        assertThat(userInfo.get("username"), is("testUser"));
+        assertThat(userInfo.get("name"), is("testName"));
+        assertThat(userInfo.get("surname"), is("testSurname"));
+        assertThat(userInfo.get("patronymic"), is("testPatronymic"));
+        assertThat((List<String>) userInfo.get("roles"), hasItem("testRoleCode1"));
+        assertThat((List<String>) userInfo.get("roles"), hasItem("testRoleCode2"));
+        assertThat((List<String>) userInfo.get("permissions"), hasItem("testPermission1"));
+        assertThat((List<String>) userInfo.get("permissions"), hasItem("testPermission2"));
+        assertThat(userInfo.get("sid"), notNullValue());
+
+    }
+
+    private static String extractQueryParameter(URI uri, String paramName) {
+        return URLEncodedUtils.parse(uri, StandardCharsets.UTF_8).stream()
+                .filter(param -> paramName.equals(param.getName()))
+                .findFirst()
+                .map(NameValuePair::getValue)
+                .orElse(null);
     }
 
     private Client client() {
@@ -93,4 +194,45 @@ public class AuthProcessingTest {
         client.setRedirectUris("*");
         return client;
     }
+
+    private User user() {
+        User user = new User();
+        user.setUsername("testUser");
+        user.setEmail("testEmail");
+        user.setName("testName");
+        user.setSurname("testSurname");
+        user.setPatronymic("testPatronymic");
+        user.setRoles(Stream.of(1, 2).map(id -> {
+            Role role = new Role();
+            role.setId(id);
+            role.setName("testRoleName" + id);
+            role.setCode("testRoleCode" + id);
+            Permission p = new Permission();
+            p.setCode("testPermission" + id);
+            role.setPermissions(List.of(p));
+            return role;
+        }).collect(Collectors.toList()));
+        return user;
+    }
+
+    private UserEntity userEntity() {
+        UserEntity user = new UserEntity();
+        user.setUsername("testUser");
+        user.setEmail("testEmail");
+        user.setName("testName");
+        user.setSurname("testSurname");
+        user.setPatronymic("testPatronymic");
+        user.setRoleList(Stream.of(1, 2).map(id -> {
+            RoleEntity role = new RoleEntity();
+            role.setId(id);
+            role.setName("testRoleName" + id);
+            role.setCode("testRoleCode" + id);
+            PermissionEntity p = new PermissionEntity();
+            p.setCode("testPermission" + id);
+            role.setPermissionList(List.of(p));
+            return role;
+        }).collect(Collectors.toList()));
+        return user;
+    }
+
 }
