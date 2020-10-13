28c52d33319e0c060d0b3eb60e8fa84af7bfb920
Молоствов Игорь <imolostvov@i-novus.ru>
2020-10-13 16:28:00 +0300

    [BF][SECURITY-253] метод getRdmSyncRest больше не принимает в качестве аргумента логер

--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/scheduled/SynchronizeJob.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/scheduled/SynchronizeJob.java
@@ -3,18 +3,12 @@ package net.n2oapp.security.admin.impl.scheduled;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.SchedulerException;
-import org.slf4j.Logger;
 import ru.inovus.ms.rdm.sync.rest.RdmSyncRest;
 
 public abstract class SynchronizeJob implements Job {
+    public static final String KEY = RdmSyncRest.class.getSimpleName();
 
-    protected RdmSyncRest getRdmSyncRest(JobExecutionContext context, Logger logger) {
-        String key = RdmSyncRest.class.getSimpleName();
-        try {
-            return (RdmSyncRest) context.getScheduler().getContext().get(key);
-        } catch (SchedulerException e) {
-            logger.error("cannot get " + key + "property", e);
-            throw new RuntimeException(e);
-        }
+    protected RdmSyncRest getRdmSyncRest(JobExecutionContext context) throws SchedulerException {
+        return (RdmSyncRest) context.getScheduler().getContext().get(KEY);
     }
 }
