650950b1046182dac39d86ecafca7c90889f84d6
btsyganov <btsyganov@i-novus.ru>
2020-10-13 13:19:12 +0300

    [rf][SECURITY-248] убрал конфигурирование в auth-server

--- auth-gateway/src/main/java/net/n2oapp/auth/gateway/AuthGatewayApplication.java
+++ auth-gateway/src/main/java/net/n2oapp/auth/gateway/AuthGatewayApplication.java
@@ -1,5 +1,6 @@
 package net.n2oapp.auth.gateway;
 
+import net.n2oapp.security.admin.auth.server.AuthGatewayConfiguration;
 import net.n2oapp.security.admin.impl.AdminImplConfiguration;
 import net.n2oapp.security.admin.rest.impl.AdminRestServerConfiguration;
 import net.n2oapp.security.admin.sso.keycloak.SsoKeycloakConfiguration;
@@ -8,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({AdminRestServerConfiguration.class, AdminImplConfiguration.class, SsoKeycloakConfiguration.class})
+@Import({AdminRestServerConfiguration.class, AdminImplConfiguration.class,
+        SsoKeycloakConfiguration.class, AuthGatewayConfiguration.class})
 public class AuthGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthGatewayApplication.class, args);
