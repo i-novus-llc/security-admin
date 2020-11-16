4e8616a5af06579af813d4a409f5e3344ea43812
Молоствов Игорь <imolostvov@i-novus.ru>
2020-11-16 10:00:40 +0300

    [BF][SECURITY-293] Удалил неиспользуемый метод в RegionRepository

--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/repository/RegionRepository.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/repository/RegionRepository.java
@@ -3,16 +3,11 @@ package net.n2oapp.security.admin.impl.repository;
 import net.n2oapp.security.admin.impl.entity.RegionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 /**
  * Репозиторий ролей
  */
 @Repository
 public interface RegionRepository extends JpaRepository<RegionEntity, Integer>, JpaSpecificationExecutor<RegionEntity> {
-    @Query("from RegionEntity r where r.code = ?1 and r.isDeleted <> true")
-    Optional<RegionEntity> findByCode(String code);
 }
