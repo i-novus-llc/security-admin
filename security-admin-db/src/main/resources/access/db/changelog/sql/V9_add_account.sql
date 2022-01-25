e9e4a80cd359b234b60a62262f6d7914663be862
aarifullin <aarifullin@i-novus.ru>
2022-01-25 17:31:45 +0300

    [NF][SECURITY-398] добавил комментарии к столбцам

--- security-admin-db/src/main/resources/access/db/changelog/sql/V9_add_account.sql
+++ security-admin-db/src/main/resources/access/db/changelog/sql/V9_add_account.sql
@@ -15,6 +15,19 @@ CREATE TABLE sec.account
     CONSTRAINT account_organization_fk FOREIGN KEY (organization_id) references sec.organization (id)
 );
 
+COMMENT ON TABLE sec.account IS 'Аккаунты пользователя';
+
+COMMENT ON COLUMN sec.account.user_id IS 'Идентификатор пользователя которому принадлежит аккаунт';
+COMMENT ON COLUMN sec.account.name IS 'Имя аккаунта';
+COMMENT ON COLUMN sec.account.region_id IS 'Идентификатор региона';
+COMMENT ON COLUMN sec.account.organization_id IS 'Идентификатор организации';
+COMMENT ON COLUMN sec.account.department_id IS 'Идентификатор департамента';
+COMMENT ON COLUMN sec.account.user_level IS 'Уровень пользователя';
+COMMENT ON COLUMN sec.account.external_system IS 'Наименование внешней системы';
+COMMENT ON COLUMN sec.account.external_uid IS 'UID пользователя во внешней системе';
+COMMENT ON COLUMN sec.account.is_active IS 'Признак активности аккаунта';
+
+
 CREATE TABLE sec.account_role
 (
     role_id    integer NOT NULL,
