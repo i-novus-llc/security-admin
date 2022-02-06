2147a6dea59b4e3026fa00c84c5c62f5e9215948
ilebedenko <ilebedenko@i-novus.ru>
2022-02-07 00:17:38 +0300

    [CF][SECURITY-407] - add some specifications

--- security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/service/specification/UserSpecifications.java
+++ security-admin-impl/src/main/java/net/n2oapp/security/admin/impl/service/specification/UserSpecifications.java
@@ -2,14 +2,10 @@ package net.n2oapp.security.admin.impl.service.specification;
 
 import net.n2oapp.security.admin.api.criteria.UserCriteria;
 import net.n2oapp.security.admin.api.model.UserLevel;
-import net.n2oapp.security.admin.impl.entity.UserEntity;
-import net.n2oapp.security.admin.impl.entity.UserEntity_;
+import net.n2oapp.security.admin.impl.entity.*;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.Arrays;
 
 import static java.util.Objects.nonNull;
@@ -28,10 +24,20 @@ public class UserSpecifications implements Specification<UserEntity> {
     @Override
     public Predicate toPredicate(Root<UserEntity> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder builder) {
         Predicate predicate = builder.and();
+        // account subquery
+        Subquery<AccountEntity> subquery = criteriaQuery.subquery(AccountEntity.class);
+        Root<AccountEntity> subqueryRoot = subquery.from(AccountEntity.class);
+        subquery.select(subqueryRoot);
+        Predicate subQueryPredicate = builder.and();
+        subQueryPredicate = builder.and(subQueryPredicate, builder.equal(root.get(UserEntity_.id),
+                subqueryRoot.get(AccountEntity_.USER).get(UserEntity_.ID)));
+
         if (nonNull(criteria.getUsername()))
             predicate = builder.and(predicate, builder.equal(root.get(UserEntity_.username), criteria.getUsername()));
+
         if (nonNull(criteria.getEmail()))
             predicate = builder.and(predicate, builder.like(root.get(UserEntity_.email), "%" + criteria.getEmail() + "%"));
+
         if (nonNull(criteria.getFio())) {
             criteria.setFio(criteria.getFio().toLowerCase().replace(" ", ""));
             predicate = builder.and(predicate,
@@ -44,6 +50,7 @@ public class UserSpecifications implements Specification<UserEntity> {
                                     builder.concat(builder.coalesce(builder.lower(builder.trim(root.get(UserEntity_.name))), ""),
                                             builder.coalesce(builder.lower(builder.trim(root.get(UserEntity_.patronymic))), ""))), criteria.getFio() + "%")));
         }
+
         if (nonNull(criteria.getIsActive())) {
             if (criteria.getIsActive().equals("yes")) {
                 predicate = builder.and(predicate, builder.equal(root.get(UserEntity_.isActive), true));
@@ -88,23 +95,29 @@ public class UserSpecifications implements Specification<UserEntity> {
         if (nonNull(criteria.getUserLevel())) {
             String userLevel = criteria.getUserLevel().toUpperCase();
             // если userLevel не существует, то возвращаем false-предикат
-            if (Arrays.stream(UserLevel.values()).map(UserLevel::getName).noneMatch(u -> u.equals(userLevel))) {
+            if (Arrays.stream(UserLevel.values()).map(UserLevel::getName).noneMatch(u -> u.equals(userLevel)))
                 return builder.disjunction();
-            }
-            //            todo SECURITY-396
-//            if (UserLevel.NOT_SET.getName().equals(userLevel)) {
-//                predicate = builder.and(predicate, builder.isNull(root.get(UserEntity_.userLevel)));
-//            } else {
-//                predicate = builder.and(predicate, builder.equal(root.get(UserEntity_.userLevel), UserLevel.valueOf(userLevel)));
-//            }
+
+            if (UserLevel.NOT_SET.getName().equals(userLevel))
+                subQueryPredicate = builder.and(subQueryPredicate,
+                        builder.isNull(subqueryRoot.get(AccountEntity_.USER_LEVEL)));
+            else
+                subQueryPredicate = builder.and(subQueryPredicate,
+                        builder.equal(subqueryRoot.get(AccountEntity_.USER_LEVEL), UserLevel.valueOf(userLevel)));
         }
-        //            todo SECURITY-396
-//        if (nonNull(criteria.getRegionId())) {
-//            predicate = builder.and(predicate, builder.equal(root.get(UserEntity_.region).get(RegionEntity_.id), criteria.getRegionId()));
-//        }
-//        if (nonNull(criteria.getDepartmentId())) {
-//            predicate = builder.and(predicate, builder.equal(root.get(UserEntity_.department).get(DepartmentEntity_.id), criteria.getDepartmentId()));
-//        }
+
+        if (nonNull(criteria.getRegionId())) {
+            subQueryPredicate = builder.and(subQueryPredicate,
+                    builder.equal(subqueryRoot.get(AccountEntity_.REGION).get(RegionEntity_.ID), criteria.getRegionId()));
+        }
+
+        if (nonNull(criteria.getDepartmentId())) {
+            subQueryPredicate = builder.and(subQueryPredicate,
+                    builder.equal(subqueryRoot.get(AccountEntity_.DEPARTMENT).get(DepartmentEntity_.ID), criteria.getDepartmentId()));
+        }
+
+        // TODO Для организаций в фильтре таблицы используется не мульти, а сингл режим, а здесь обрабатывается, как мульти
+        //    В чем смысл, Карл?
 //        if (!CollectionUtils.isEmpty(criteria.getOrganizations())) {
 //            CriteriaBuilder.In<Integer> in = builder.in(root.get(UserEntity_.organization).get(OrganizationEntity_.id));
 //            criteria.getOrganizations().forEach(in::value);
@@ -113,6 +126,9 @@ public class UserSpecifications implements Specification<UserEntity> {
         if (nonNull(criteria.getLastActionDate())) {
             predicate = builder.and(predicate, builder.greaterThanOrEqualTo(root.get(UserEntity_.lastActionDate), criteria.getLastActionDate()));
         }
+
+        subquery.select(subqueryRoot).where(subQueryPredicate);
+        predicate = builder.and(predicate, builder.exists(subquery));
         return predicate;
     }
 }
