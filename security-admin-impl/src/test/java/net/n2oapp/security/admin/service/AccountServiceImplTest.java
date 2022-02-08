6979276a4a9f3a6cfe3bc131e56ca467441f9514
ilebedenko <ilebedenko@i-novus.ru>
2022-02-08 23:07:27 +0300

    [CF][SECURITY-407] - add account service tests

--- security-admin-impl/src/test/java/net/n2oapp/security/admin/service/AccountServiceImplTest.java
+++ security-admin-impl/src/test/java/net/n2oapp/security/admin/service/AccountServiceImplTest.java
@@ -1,9 +1,9 @@
 package net.n2oapp.security.admin.service;
 
 import net.n2oapp.platform.i18n.UserException;
+import net.n2oapp.security.admin.api.criteria.AccountCriteria;
 import net.n2oapp.security.admin.api.model.*;
 import net.n2oapp.security.admin.api.service.AccountService;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +12,12 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
- * Тест сервиса управления правами доступа
+ * Тест сервиса управления аккаунтами
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -27,28 +28,31 @@ public class AccountServiceImplTest {
     private AccountService service;
 
     @Test
-    public void testUp() throws Exception {
+    public void testUp() {
         assertNotNull(service);
     }
 
-
     @Test
-    public void createTest() {
+    public void findAllTest() {
+        AccountCriteria criteria = new AccountCriteria();
 
+        List<Account> accounts = service.findAll(criteria).getContent();
+        assertEquals(2, accounts.size());
+        assertEquals(1, (int) accounts.get(0).getId());
+        assertEquals(2, (int) accounts.get(1).getId());
+
+        criteria.setUserId(2);
+        accounts = service.findAll(criteria).getContent();
+        assertEquals(1, accounts.size());
+        assertEquals(2, (int) accounts.get(0).getId());
+        assertEquals(2, (int) accounts.get(0).getUserId());
     }
 
     @Test
-    public void updateTest() {
-
-    }
-
-    @Test
-    @Ignore
-    public void deleteTest() {
-        Integer accountId = service.create(getAccount()).getId();
-        assertNotNull(service.getById(accountId));
-        service.delete(accountId);
-        assertThrows(UserException.class, () -> service.getById(accountId));
+    public void crudTest() {
+        Integer accountId = create();
+        update(accountId);
+        delete(accountId);
     }
 
     @Test(expected = UserException.class)
@@ -68,7 +72,83 @@ public class AccountServiceImplTest {
         service.delete(-1);
     }
 
-    private Account getAccount() {
+    private Integer create() {
+        Account newAccount = createAccount();
+        Integer createdAccountId = service.create(newAccount).getId();
+        Account account = service.getById(createdAccountId);
+
+        assertEquals(newAccount.getName(), account.getName());
+        assertEquals(newAccount.getIsActive(), account.getIsActive());
+        assertEquals(newAccount.getUserId(), account.getUserId());
+        assertEquals(newAccount.getUserLevel(), account.getUserLevel());
+
+        assertNotNull(account.getDepartment());
+        assertEquals("test_name", account.getDepartment().getName());
+        assertEquals("test_code", account.getDepartment().getCode());
+
+        assertNotNull(account.getOrganization());
+        assertEquals("test_code2", account.getOrganization().getCode());
+        assertEquals("test_short_name2", account.getOrganization().getShortName());
+        assertEquals("test_full_name2", account.getOrganization().getFullName());
+        assertEquals("test_inn2", account.getOrganization().getInn());
+
+        assertNotNull(account.getRegion());
+        assertEquals("test_name", account.getRegion().getName());
+        assertEquals("test_code", account.getRegion().getCode());
+        assertEquals("test_okato", account.getRegion().getOkato());
+
+        assertEquals(1, account.getRoles().size());
+        assertEquals("admin", account.getRoles().get(0).getName());
+        assertEquals("code2", account.getRoles().get(0).getCode());
+        assertEquals("description2", account.getRoles().get(0).getDescription());
+
+        return createdAccountId;
+    }
+
+    private void update(Integer accountId) {
+        Account updatedAccount = new Account();
+        updatedAccount.setId(accountId);
+        updatedAccount.setName("new_name");
+        updatedAccount.setIsActive(true);
+        updatedAccount.setUserLevel(UserLevel.ORGANIZATION);
+        Organization org = new Organization();
+        org.setId(3);
+        updatedAccount.setOrganization(org);
+        Role role = new Role();
+        role.setId(1);
+        updatedAccount.setRoles(Collections.singletonList(role));
+
+        service.update(updatedAccount);
+        Account account = service.getById(accountId);
+
+        assertEquals(updatedAccount.getName(), account.getName());
+        assertEquals(updatedAccount.getIsActive(), account.getIsActive());
+        assertEquals(updatedAccount.getUserLevel(), account.getUserLevel());
+
+        assertNull(account.getDepartment());
+
+        assertNotNull(account.getOrganization());
+        assertEquals("test_code3", account.getOrganization().getCode());
+        assertEquals("test_short_name3", account.getOrganization().getShortName());
+        assertEquals("test_full_name3", account.getOrganization().getFullName());
+        assertEquals("test_inn3", account.getOrganization().getInn());
+
+        assertNull(account.getRegion());
+
+        assertEquals(1, account.getRoles().size());
+        assertEquals("user", account.getRoles().get(0).getName());
+        assertEquals("code1", account.getRoles().get(0).getCode());
+        assertEquals("description1", account.getRoles().get(0).getDescription());
+    }
+
+    private void delete(Integer accountId) {
+        assertNotNull(service.getById(accountId));
+        service.delete(accountId);
+        assertThrows(UserException.class, () -> service.getById(accountId));
+    }
+
+
+    private Account createAccount() {
         Account account = new Account();
         account.setName("testAccount");
         account.setIsActive(false);
