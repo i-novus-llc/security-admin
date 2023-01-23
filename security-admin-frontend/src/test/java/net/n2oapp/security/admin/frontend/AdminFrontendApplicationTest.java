2c91d8dfdd51c742a00943c0d040a2179c72c99c
aarifullin <aarifullin@i-novus.ru>
2023-01-23 17:11:41 +0300

    [SECURITY-424] fix test

--- security-admin-frontend/src/test/java/net/n2oapp/security/admin/frontend/AdminFrontendApplicationTest.java
+++ security-admin-frontend/src/test/java/net/n2oapp/security/admin/frontend/AdminFrontendApplicationTest.java
@@ -8,7 +8,10 @@ import org.springframework.context.ApplicationContext;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.security.oauth2.client.registration.admin-web.client-secret=fbcf4023-afe0-4ba1-944e-7a0f675b0b98",
+        "spring.security.oauth2.client.registration.admin-web.client-id=admin-web",
+        "access.keycloak.server-url=https://keycloak8.i-novus.ru/auth"})
 public class AdminFrontendApplicationTest {
 
     @Autowired
