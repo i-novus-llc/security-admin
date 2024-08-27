c5085db8bb4a6d4825244c1d242ba5ee82f17342
aarifullin <aarifullin@i-novus.ru>
2024-08-27 13:55:01 +0300

    EGISZNRPLATFORM-1740 remove rdm for update
    Task: https://jira.i-novus.ru/browse/EGISZNRPLATFORM-1740

--- security-admin-backend/src/test/java/net/n2oapp/security/admin/TestApplication.java
+++ security-admin-backend/src/test/java/net/n2oapp/security/admin/TestApplication.java
@@ -1,35 +1,31 @@
 package net.n2oapp.security.admin;
 
-import net.n2oapp.platform.test.autoconfigure.pg.EnableEmbeddedPg;
+import net.n2oapp.platform.test.autoconfigure.pg.EnableTestcontainersPg;
+import net.n2oapp.platform.test.autoconfigure.pg.TestcontainersPgAutoConfiguration;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import ru.i_novus.ms.rdm.api.service.DraftService;
-import ru.i_novus.ms.rdm.api.service.PublishService;
-import ru.i_novus.ms.rdm.api.service.RefBookService;
-import ru.i_novus.ms.rdm.api.service.VersionService;
-import ru.i_novus.ms.rdm.sync.RdmClientSyncAutoConfiguration;
-import ru.i_novus.ms.rdm.sync.service.change_data.RdmChangeDataClient;
+import org.springframework.context.annotation.Import;
 
 /**
  * Стартовая точка запуска Spring Boot
  */
-@SpringBootApplication(exclude = RdmClientSyncAutoConfiguration.class)
-@EnableEmbeddedPg
+@SpringBootApplication()
+@EnableTestcontainersPg
+@Import(TestcontainersPgAutoConfiguration.class)
 public class TestApplication {
 
-    @MockBean
-    private VersionService versionService;
-    @MockBean
-    private RefBookService refBookService;
-    @MockBean
-    private DraftService draftService;
-    @MockBean
-    private PublishService publishService;
-    @MockBean
-    private RdmChangeDataClient rdmChangeDataClient;
+//    @MockBean
+//    private VersionService versionService;
+//    @MockBean
+//    private RefBookService refBookService;
+//    @MockBean
+//    private DraftService draftService;
+//    @MockBean
+//    private PublishService publishService;
+//    @MockBean
+//    private RdmChangeDataClient rdmChangeDataClient;
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
