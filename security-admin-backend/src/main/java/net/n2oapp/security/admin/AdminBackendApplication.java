74dde44c770b05b424fcd5ba24ca6b5bd537a526
aarifullin <aarifullin@i-novus.ru>
2025-08-15 16:12:07 +0300

    java 21, n2o platform 7.0.0, springboot 3.5.3
    Task: https://jira.i-novus.ru/browse/SECURITY-486

--- security-admin-backend/src/main/java/net/n2oapp/security/admin/AdminBackendApplication.java
+++ security-admin-backend/src/main/java/net/n2oapp/security/admin/AdminBackendApplication.java
@@ -4,11 +4,11 @@ import net.n2oapp.security.admin.rest.impl.AdminRestServerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class, OAuth2ClientWebSecurityAutoConfiguration.class})
 @Import({AdminRestServerConfiguration.class})
 public class AdminBackendApplication {
     public static void main(String[] args) {
