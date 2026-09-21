1e5f879ede3bee95c358adf48b66a9ead53ef177
aarifullin <aarifullin@i-novus.ru>
2026-09-21 16:14:20 +0300

    Remove redundant N2oQueryProcessor override
    Task: https://jira.i-novus.ru/browse/SECURITY-513

--- security-admin-web/src/main/java/net/n2oapp/security/admin/web/AdminWebConfiguration.java
+++ security-admin-web/src/main/java/net/n2oapp/security/admin/web/AdminWebConfiguration.java
@@ -1,29 +1,15 @@
 package net.n2oapp.security.admin.web;
 
-import net.n2oapp.framework.api.MetadataEnvironment;
-import net.n2oapp.framework.api.data.CriteriaConstructorFactory;
-import net.n2oapp.framework.api.data.QueryExceptionHandler;
-import net.n2oapp.framework.api.data.QueryProcessor;
-import net.n2oapp.framework.engine.data.N2oInvocationFactory;
-import net.n2oapp.framework.engine.data.N2oQueryProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class AdminWebConfiguration {
 
     @Bean
-    public QueryProcessor saQueryProcessor(MetadataEnvironment environment,
-                                           N2oInvocationFactory invocationFactory,
-                                           QueryExceptionHandler exceptionHandler) {
-        N2oQueryProcessor queryProcessor = new N2oQueryProcessor(invocationFactory, exceptionHandler);
-        queryProcessor.setCriteriaConstructorFactory(new CriteriaConstructorFactory(List.of(new BaseCriteriaConstructor())));
-        queryProcessor.setPageStartsWith0(true);
-        queryProcessor.setEnvironment(environment);
-        return queryProcessor;
+    public BaseCriteriaConstructor baseCriteriaConstructor() {
+        return new BaseCriteriaConstructor();
     }
 
     @Configuration
